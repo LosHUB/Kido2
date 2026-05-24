229bf3b0b01da6f144d7e43a71ea1e0af90fcab8
LosHUB <123419422+LosHUB@users.noreply.github.com>
2026-05-24 14:43:06 +0200

    Add files via upload

---
+++ KidoLoadingScreen/config.js
@@ -0,0 +1,66 @@
+const Config = {
+    backgrounds: [
+        { url: "1363137.png", particle: "sakura" },
+        { url: "1399041.jpg", particle: "dark" },
+        { url: "1362335.jpeg", particle: "fire" }
+    ],
+    backgroundDelay: 10000,
+
+    music: [
+        "Suzume_No_Tojimari.mp3"
+    ],
+    musicVolume: 0.02,
+
+
+    features: [
+        {
+            title: "IMMERSION TOTALE",
+            desc: "Un lore respecté, des événements dynamiques et une communauté soudée."
+        },
+        {
+            title: "HISTOIRE DE KIDO",
+            desc: "Kido Community a officiellement ouvert ses portes le 9 juin 2023. Bientôt 3 ans d'histoire, de RP et de guerre entre Pourfendeurs et Démons."
+        },
+        {
+            title: "COMMUNAUTÉ ACTIVE",
+            desc: "Le serveur continue d'évoluer grâce à sa communauté et aux nombreux joueurs qui construisent le RP chaque jour."
+        },
+        {
+            title: "SERIOUS RP",
+            desc: "Kido est un serveur Serious RP. Vos actions, vos choix et vos relations ont de réelles conséquences sur l'univers."
+        },
+        {
+            title: "LA FONDATION",
+            desc: "Le serveur est actuellement géré par Nauja et L."
+        },
+        {
+            title: "LE RP AVANT LE PVP",
+            desc: "Le RP ne se limite pas au PvP. Discussions, tensions, enquêtes, rivalités et scènes du quotidien font vivre le serveur."
+        },
+        {
+            title: "L'ÉVOLUTION DU LORE",
+            desc: "Les grandes figures du serveur changent avec le temps. Certains tombent, d'autres prennent leur place."
+        },
+        {
+            title: "DÉVELOPPEMENT UNIQUE",
+            desc: "Plus de 90% des systèmes du serveur ont été développés ou modifiés spécialement pour Kido."
+        },
+        {
+            title: "RP À CONSÉQUENCES",
+            desc: "Le serveur privilégie un RP à conséquences. Prenez soin de votre personnage et réfléchissez avant d'agir."
+        },
+        {
+            title: "LES FACTIONS",
+            desc: "Chaque faction possède ses propres ambitions, secrets et conflits internes. Faites évoluer votre histoire."
+        },
+        {
+            title: "REMERCIEMENTS",
+            desc: "Merci à tous les joueurs ayant participé au projet depuis ses débuts."
+        },
+        {
+            title: "LES COULISSES DE KIDO",
+            desc: "Kido n'aurait jamais tenu aussi longtemps sans l'investissement de ses staffs, animateurs, développeurs et joueurs ayant contribué au projet au fil des années."
+        }
+    ],
+    featureDelay: 6000
+};
